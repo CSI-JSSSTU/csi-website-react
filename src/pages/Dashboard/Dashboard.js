5c721f76d1fed59ea56225b02e3f7f6ef0fd9357
CSI-JSSSTU <csi@jssstuniv.in>
2024-10-15 21:08:19 +0530

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,114 +1,58 @@
 import React from "react";
 import './Dashboard.css';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import Grid from '@mui/material/Grid'; 
 import { useNavigate } from "react-router-dom";
 
+const services = [
+    {
+        id: '/zenith/aic',
+        title: 'ANDROID DEVELOPMENT',
+        image: '/images/android.jpg',
+    },
+    {
+        id: '/zenith/ml',
+        title: 'MACHINE LEARNING',
+        image: '/images/ml.jpg',
+    },
+    {
+        id: '/zenith/riot',
+        title: 'ROBOTICS & IOT',
+        image: '/images/iot.jpg',
+    },
+    {
+        id: '/zenith/vlsi',
+        title: 'BLOCKCHAIN DEVELOPMENT',
+        image: '/images/vlsi.webp',
+    },
+    {
+        id: '/zenith/wd',
+        title: 'WEB DEVELOPMENT',
+        image: '/images/webdev.png',
+    }
+];
+
 const Dashboard = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const onSelect = (id) => {
-        navigate(id)
-        window.scrollTo(0,0)
-    }
-    return(
-        <div className="dashboard" >
-            <Grid container rowSpacing={{ xs: 1, sm: 2, md: 3 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="center">
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card onClick={() => onSelect('/zenith/aic')}>
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            image="/images/android.jpg"
-                            alt="Android Development"
-                            sx={{ height: "200px" }}
-                            />
-                            <CardContent sx={{backgroundColor: 'black'}}>
-                                <Typography gutterBottom variant="h5" component="div" textAlign="center" fontWeight="bolder" color="white" fontFamily="Raleway">
-                                    ANDROID DEVELOPMENT
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card onClick={() => onSelect('/zenith/ml')}>
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            image="/images/ml.jpg"
-                            alt="machine learning"
-                            sx={{ height: "200px" }}
-                            />
-                            <CardContent sx={{backgroundColor: 'black'}}>
-                            <Typography gutterBottom variant="h5" component="div" textAlign="center" fontWeight="bolder" color="white" fontFamily="Raleway">
-                                MACHINE LEARNING
-                            </Typography>
-                            
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card onClick={() => onSelect('/zenith/riot')} >
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            sx={{ height: "200px" }}
-                            image="/images/iot.jpg"
-                            alt="robotics and iot"
-                            />
-                            <CardContent sx={{backgroundColor: 'black'}}>
-                            <Typography gutterBottom variant="h5" component="div" textAlign="center" fontWeight="bolder" color="white" fontFamily="Raleway">
-                                ROBOTICS & IOT
-                            </Typography>
-                            
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card onClick={() => onSelect('/zenith/vlsi')}>
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            sx={{ height: "200px" }}
-                            image="/images/vlsi.webp"
-                            alt="Blockchain Development"
-                            />
-                            <CardContent sx={{backgroundColor: 'black'}}>
-                            <Typography gutterBottom variant="h5" component="div" textAlign="center" fontWeight="bolder" color="white" fontFamily="Raleway" >
-                                BLOCKCHAIN DEVELOPMENT
-                            </Typography>
-                            
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <Card onClick={() => onSelect('/zenith/wd')}>
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            sx={{ height: "200px" }}
-                            image="/images/webdev.png"
-                            alt="Web Development"
-                            />
-                            <CardContent sx={{backgroundColor: 'black'}}>
-                            <Typography gutterBottom variant="h5" component="div" textAlign="center" fontWeight="bolder" color="white" fontFamily="Raleway">
-                                WEB DEVELOPMENT
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            </Grid>
+        navigate(id);
+        window.scrollTo(0, 0);
+    };
+
+    return (
+        <div className="dashboard">
+            {services.map((item) => (
+                <div className="card" key={item.title} onClick={() => onSelect(item.id)}>
+                    <div
+                        className="card-image"
+                        style={{ backgroundImage: `url(${item.image})` }}
+                    />
+                    <div className="card-content">
+                        <h3>{item.title}</h3>
+                    </div>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default Dashboard;
