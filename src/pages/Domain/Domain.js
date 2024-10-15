4471403b582c658e8cba5d593b95c39b593b3d39
CSI-JSSSTU <csi@jssstuniv.in>
2024-10-15 21:07:58 +0530

    Update Domain.js

--- src/pages/Domain/Domain.js
+++ src/pages/Domain/Domain.js
@@ -1,58 +1,63 @@
-import React from "react";
-import './Dashboard.css';
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const services = [
-    {
-        id: '/zenith/aic',
-        title: 'ANDROID DEVELOPMENT',
-        image: '/images/android.jpg',
-    },
-    {
-        id: '/zenith/ml',
-        title: 'MACHINE LEARNING',
-        image: '/images/ml.jpg',
-    },
-    {
-        id: '/zenith/riot',
-        title: 'ROBOTICS & IOT',
-        image: '/images/iot.jpg',
-    },
-    {
-        id: '/zenith/vlsi',
-        title: 'BLOCKCHAIN DEVELOPMENT',
-        image: '/images/vlsi.webp',
-    },
-    {
-        id: '/zenith/wd',
-        title: 'WEB DEVELOPMENT',
-        image: '/images/webdev.png',
-    }
-];
+const Domain  = () => {
+    const params = useParams()
+    const domainId = params.domainId
 
-const Dashboard = () => {
-    const navigate = useNavigate();
+    const domains = {aic: 'Analog IC', ml: 'Machine Learning', riot: 'Robotics & IoT', vlsi: 'VLSI', wd: 'Web Development'}
+    return(
+        <div className="blogs">
+            {/* Title  */}
+            <div className="title">
+                <span className="title-span">{domains[domainId]}</span>
+            </div>
 
-    const onSelect = (id) => {
-        navigate(id);
-        window.scrollTo(0, 0);
-    };
-
-    return (
-        <div className="dashboard">
-            {services.map((item) => (
-                <div className="card" key={item.title} onClick={() => onSelect(item.id)}>
-                    <div
-                        className="card-image"
-                        style={{ backgroundImage: `url(${item.image})` }}
-                    />
-                    <div className="card-content">
-                        <h3>{item.title}</h3>
-                    </div>
+            {/* Content */}
+            <section className="blog" id="b1">
+                <div className="blog-number">
+                session#<span className="b-number">1</span>
                 </div>
-            ))}
-        </div>
-    );
-};
+                <div className="blog-title">Session Title</div>
+                <div className="blog-content">
+                <p>
+                    A para about the session. (optional) Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Slides Link: <span className="content-style">Link to Slides</span>
+                </p>
+                <p>
+                    Resources Link: <span className="content-style">Link to youtube/ website</span>
+                </p>
+                </div>
 
-export default Dashboard;
+                <br />
+                <br />
+                <div className="blog-number">
+                session#<span className="b-number">2</span>
+                </div>
+                <div className="blog-title">Session Title</div>
+                <div className="blog-content">
+                <p>
+                    A para about the session. (optional) Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Slides Link: <span className="content-style">Link to Slides</span>
+                </p>
+                <p>
+                    Resources Link: <span className="content-style">Link to youtube/ website</span>
+                </p>
+                </div>
+            </section>
+            
+        </div>
+    )
+}
+
+export default Domain
